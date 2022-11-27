d55c01028fe2d4ed22ae659d488e987be8f78a6a
Ronald Requena <rcronald@gmail.com>
2022-11-27 01:45:43 -0500

    Update tools_install.sh

--- scripts/tools_install.sh
+++ scripts/tools_install.sh
@@ -6,6 +6,9 @@ curl -sS https://webinstall.dev/k9s | bash
 ## Install helm
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
+## Mathias
+git clone https://github.com/mathiasbynens/dotfiles.git && cd dotfiles && set -- -f; source bootstrap.sh
+
 ## Install wrk
 brew install wrk
 
