cf20c580b1e9e9f10ec97aec5a92d17319766a80
Ronald Requena <rcronald@gmail.com>
2022-11-27 02:10:54 -0500

    Update tools_install.sh

--- scripts/tools_install.sh
+++ scripts/tools_install.sh
@@ -5,6 +5,3 @@ curl -sS https://webinstall.dev/k9s | bash
 
 ## Install helm
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
-
-## Mathias
-curl -#L https://github.com/mathiasbynens/dotfiles/tarball/main | tar -xzv --strip-components 1 --exclude={README.md,.osx,LICENSE-MIT.txt} && set -- -f; source bootstrap.sh
