40b96e392e152f248106c815c7b342a665be174e
Ronald Requena <rcronald@gmail.com>
2022-11-27 02:24:28 -0500

    Update terraform_configure.sh

--- scripts/terraform_configure.sh
+++ scripts/terraform_configure.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 
-if command -v /usr/bin/terraform &> /dev/null; then
-    exit
-fi
+#if command -v /usr/bin/terraform &> /dev/null; then
+#    exit
+#fi
 
-sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
+#sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
 
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+#curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 
-sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+#sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 
-sudo apt-get update && sudo apt-get install terraform
+#sudo apt-get update && sudo apt-get install terraform
